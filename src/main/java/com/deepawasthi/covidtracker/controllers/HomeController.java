b96eff6ee15acdf02a31eb4d022298725fb327ee
Deep Awasthi <da.madskull@gmail.com>
2023-03-22 13:21:40 +0530

    Setting model to controller

--- src/main/java/com/deepawasthi/covidtracker/controllers/HomeController.java
+++ src/main/java/com/deepawasthi/covidtracker/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.deepawasthi.covidtracker.controllers;
 
+import com.deepawasthi.covidtracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
     @GetMapping("/home")
     public String home(Model model){
-        model.addAttribute("testName", "TEST");
+        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
         return "home";
     }
 }
