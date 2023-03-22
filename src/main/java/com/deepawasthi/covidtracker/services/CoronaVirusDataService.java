77aa5b8d6042e8fd288243272e1cd1382bf5d756
Deep Awasthi <da.madskull@gmail.com>
2023-03-22 13:18:06 +0530

    Setting up model and controller

--- src/main/java/com/deepawasthi/covidtracker/services/CoronaVirusDataService.java
+++ src/main/java/com/deepawasthi/covidtracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package com.deepawasthi.covidtracker.services;
 
+import com.deepawasthi.covidtracker.models.LocationStats;
 import jakarta.annotation.PostConstruct;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -12,13 +13,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    private List<LocationStats> allStats = new ArrayList<>();
+
     @PostConstruct
-    @Scheduled(cron = "* * * * * *")
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request =  HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
@@ -27,9 +35,15 @@ public class CoronaVirusDataService {
 
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(csvBodyReader);
+
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+
+            newStats.add(locationStat);
         }
+        this.allStats = newStats;
     }
 }
